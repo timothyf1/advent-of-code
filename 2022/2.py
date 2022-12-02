89468457db5ba44da0696046bcfb49e04f16ee46
Timothy Standen <timothyf1@live.co.uk>
2022-12-02 19:07:47 +0000

    bit more efficecnt

--- 2022/2.py
+++ 2022/2.py
@@ -14,62 +14,39 @@ inp = [i.split(" ") for i in inp][:-1]
 
 
 score = 0
+new_score = 0
+
 for line in inp:
     
     if line[1] == "X":
         if line[0] == "A":
             score += 4
+            new_score += 3
         if line[0] == "B":
             score += 1
+            new_score += 1
         if line[0] == "C":
             score += 7
+            new_score += 2
     if line[1] == "Y":
         if line[0] == "A":
             score += 8
+            new_score += 4
         if line[0] == "B":
             score += 5
+            new_score += 5
         if line[0] == "C":
             score += 2
+            new_score += 6
     if line[1] == "Z":
         if line[0] == "A":
             score += 3
-        if line[0] == "B":
-            score += 9
-        if line[0] == "C":
-            score += 6
-
-
-# dic1 = {
-#     ("A", "X") :  
-# }
-
-new_score = 0
-for line in inp:
-    # Need to lose
-    if line[1] == "X":
-        if line[0] == "A":
-            new_score += 3
-        if line[0] == "B":
-            new_score += 1
-        if line[0] == "C":
-            new_score += 2
-
-    # Need to draw
-    if line[1] == "Y":
-        if line[0] == "A":
-            new_score += 4
-        if line[0] == "B":
-            new_score += 5
-        if line[0] == "C":
-            new_score += 6
-
-    # Need to win
-    if line[1] == "Z":
-        if line[0] == "A":
             new_score += 8
         if line[0] == "B":
+            score += 9
             new_score += 9
         if line[0] == "C":
+            score += 6
             new_score += 7
 
 

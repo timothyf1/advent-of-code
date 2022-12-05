cdad93cbb4abaa3c0123ae960ee042d1a392f7e9
Timothy Standen <timothyf1@live.co.uk>
2022-12-05 08:29:16 +0000

    day 5

---
+++ 2022/5.py
@@ -0,0 +1,43 @@
+import copy
+
+with open("input-files/day5.txt") as f:
+    data = f.read()
+
+data = data.split("\n\n")
+
+stacks_data = data[0].split("\n")
+moves = [i.split(" ") for i in data[1].split("\n")][:-1]
+moves = [[int(i) for i in j if i.isdigit()] for j in moves]
+
+num_of_stacks = len([i for i in stacks_data.pop().split(" ") if i != ""])
+
+stacks = [[] for i in range(num_of_stacks)]
+
+for i in range(len(stacks_data)-1, -1, -1):
+    for j in range(num_of_stacks):
+        if stacks_data[i][j*4+1] != " ":
+            stacks[j].append(stacks_data[i][j*4+1]) 
+
+stacks_two = copy.deepcopy(stacks)
+
+for move in moves:
+    for i in range(move[0]):
+        stacks[move[2]-1].append(stacks[move[1]-1].pop())
+
+top_crates = ""
+
+for stack in stacks:
+    top_crates += stack[-1]
+
+print(top_crates)
+
+for move in moves:
+    stacks_two[move[2]-1] += stacks_two[move[1]-1][-move[0]:]
+    stacks_two[move[1]-1] = stacks_two[move[1]-1][:-move[0]]
+
+top_crates_2 = ""
+
+for stack in stacks_two:
+    top_crates_2 += stack[-1]
+
+print(top_crates_2)
